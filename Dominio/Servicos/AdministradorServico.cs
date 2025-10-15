3a00bead4aac8534a4cc6feaf6853aa61fccf76c
Guilherme Maia <xguilherme.ca.maiax@gmail.com>
2025-10-15 10:30:48 -0300

    Conexão com o banco de dados feito

---
+++ Dominio/Servicos/AdministradorServico.cs
@@ -0,0 +1,20 @@
+using MinimalApi.Dominio.Entidades;
+using MinimalApi.Dominio.Interfaces;
+using MinimalApi.DTO;
+using MinimalApi.Infraestrutura.Db;
+
+namespace MinimalApi.Dominio.Servicos;
+
+public class AdministradorServico : IAdministradorServico
+{
+    private readonly DbContexto _contexto;
+    public AdministradorServico(DbContexto contexto)
+    {
+        _contexto = contexto;
+    }
+    public Administrador? Login(LoginDTO loginDTO)
+    {
+        var adm = _contexto.administradores.Where(a => a.Email == loginDTO.Email && a.Senha == loginDTO.Senha).FirstOrDefault();
+        return adm;
+    }
+}
---
+++ Migrations/20251015131629_SeedAdministrador.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace minimal_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedAdministrador : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "administradores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Email = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Senha = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Perfil = table.Column<string>(type: "varchar(10)", maxLength: 10, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_administradores", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "administradores",
+                columns: new[] { "Id", "Email", "Nome", "Perfil", "Senha" },
+                values: new object[] { 1, "administrador@teste.com", "Administrador", "Adm", "123456" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "administradores");
+        }
+    }
+}
